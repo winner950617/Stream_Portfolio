131f10d8761de19d9a34258e3155519f7b30682c
winner950617 <winner950617@gmail.com>
2025-01-05 12:39:03 +0430

    set up

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Header from './Componets/Header/Header'
+import About from './Componets/About/About'
+import TechStack from './Componets/TechStack/TechStack'
+import Project from './Componets/Project/Project'
+import Contact from './Componets/Contact/Contact'
+import Footer from './Componets/Footer/Footer'
+import Education from './Componets/Education/Education'
+
+export default function App() {
+  return (
+    <>
+      <div className="fixed inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <main className='flex flex-col items-center px-2 md:px-4 lg:px-16'>
+        
+        <Header />
+
+        <About />
+        
+
+        <TechStack />
+
+        <Project />
+        
+        <Education />
+
+        <Contact />
+    
+      </main>
+      
+      <Footer />
+
+    </>
+  )
+}
+
---
+++ src/Data/project.js
@@ -0,0 +1,63 @@
+import weather from '../assets/weather.jpg'
+import shop from '../assets/sh.jpg'
+import bank from '../assets/bank.png'
+import mapty from '../assets/Mapty.png'
+import Lib from '../assets/Li.png'
+import pizza from '../assets/pizza.png'
+
+const projectArray=[
+    {
+        image:weather,
+        title:'Weather App',
+        description:'I developed a responsive weather app using React.js, providing real-time weather updates with an intuitive and user-friendly interface.',
+        techstack:["React","CSS",'JavaScript'],
+        dlink:"https://weather-app-delta-ebon.vercel.app/",
+        clink:"https://github.com/MOwaisAzizi/Weather-App"
+    },
+
+    {
+        image:bank,
+        title:'Bank app',
+        description:'I created an interactive bank app using HTML, CSS, and JavaScript, offering a fun and engaging experience with ability to transform money, loaon money and pay money to different accounts(4 build in acounts).',
+        techstack:["HTML","CSS","Bootstrap","JavaScript"],
+        dlink:"http://mowaisazizi.github.io/BankApplication/",
+        clink:"https://github.com/MOwaisAzizi/BankApplication"
+    },
+
+
+    {
+        image:Lib,
+        title:'Library',
+        description:'I developed a user-friendly interface Library management app to store your favorite book in Firebase and search, retrieve after comming later.',
+        techstack:["React","CSS","Firebase","JavaScript"],
+        dlink:"http://library-isos.vercel.app",
+        clink:"https://github.com/MOwaisAzizi/library"
+    },
+
+    {
+        image:mapty,
+        title:'Mapty',
+        description:'I developed a mapty website using JavaScrpt, featuring a dynamic, user-friendly interface for showing different locations and abilty to store your activity in locations.',
+        techstack:["HTML","CSS","JavaScript(OOP)"],
+        dlink:"https://bhawa2024.com",
+        
+    },
+    {
+        image:pizza,
+        title:'Pizza',
+        description:'I developed a user-friendly interface Pizza online shope in order to buy your favorite piza, put it in your basket and order it if you want.',
+        techstack:["React","CSS","tailwind"],
+        dlink:"https://fast-ract-pizza-v2.vercel.app",
+        clink:"https://github.com/MOwaisAzizi/fast-react-pizza-v2"
+    },
+    {
+        image:shop,
+        title:'Online Shope',
+        description:'I created an interactive and responsive online Shope using React.js, offering an efficient performance and ability to shope online. It feature seamless navigation, fast load times, and dynamic shope browsing.',
+        techstack:["React","CSS",'JavaScript','BootStrap'],
+        dlink:"http://online-market-phi.vercel.app",
+        clink:"https://github.com/MOwaisAzizi/Online-Market"
+    },
+]
+
+export default projectArray;
