534526bc322c252272ce9cf6b4351e545570424b
winner950617 <winner950617@gmail.com>
2025-01-05 20:55:05 +0430

    update

--- src/Componets/About/About.jsx
+++ src/Componets/About/About.jsx
@@ -37,7 +37,7 @@ function About() {
         >
           <h1
             className="bg-gradient-to-r from-blue-500 to-pink-500 
-                     bg-clip-text text-transparent text-4xl font- md:text-6xl"
+                     bg-clip-text text-transparent text-5xl font- md:text-6xl"
           >
             Mohammad Owais Azizi
           </h1>
