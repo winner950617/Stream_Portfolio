5867a0c21a3c9076caadef35c3b0336b25c5b36f
winner950617 <winner950617@gmail.com>
2025-01-06 23:33:43 +0430

    update

--- src/Componets/Header/Header.jsx
+++ src/Componets/Header/Header.jsx
@@ -150,7 +150,7 @@ function Header() {
             </a>
           </ul>
 
-          <ul className="flex flex-wrap gap-7">
+          <ul className="flex flex-wrap gap-6">
             <a
               href="https://www.linkedin.com/in/m-owais-azizi-7440532b4"
               target="_blank"
